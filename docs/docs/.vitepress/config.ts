97872d46976935d34938fcc59d1614c8b8c0be3c
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-19 19:41:27 +0900

    docs: create

---
+++ docs/docs/.vitepress/config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    title: 'PayPax JS | PayPay Library',
+    description: 'PayPay operations can be automated | PayPax JS',
+    head: [
+        ['link', { rel: 'icon', type: 'image/png', href: '../public/favicon.png' }],
+        ['meta', { name: 'og:image', content: '../public/favicon.png' }],
+        ['meta', { name: 'og:title', content: 'PayPax JS | PayPay Library' }],
+        ['meta', { name: 'og:description', content: 'PayPay operations can be automated | PayPax JS' }],
+        ['meta', { name: 'og:site_name', content: 'PayPax JS | PayPay Library' }],
+        ['meta', { name: 'og:type', content: 'website' }],
+        ['meta', { name: 'twitter:image', content: '../public/favicon.png' }],
+        ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+        ['meta', { name: 'twitter:site', content: '@amex2189' }],
+        ['meta', { name: 'twitter:creator', content: '@amex2189' }],
+        ['meta', { name: 'twitter:title', content: 'PayPax JS | PayPay Library' }],
+        ['meta', { name: 'twitter:description', content: 'PayPay operations can be automated | PayPax JS' }],
+    ],
+})
