7132756d6dc997695d85ce8007c356b95568e322
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-22 17:29:12 +0900

    add statusod method v1.7.6

--- deno_dist/status/index.ts
+++ deno_dist/status/index.ts
@@ -1,10 +1,49 @@
-export const PayPayStatus = {
-  LoginSuccess: 'LoginSuccess',
-  LoginFailed: 'LoginFailed',
-  LoginIncorrectPassOrPhone: 'LoginIncorrectPassOrPhone',
-  LoginNeedOTP: 'LoginNeedOTP',
-  LoginDontNeedOTP: 'LoginDontNeedOTP',
-  OTPLoginSuccess: 'OTPLoginSuccess',
-  OTPLoginFail: 'OTPLoginFail',
-  LoginAlreadySuccess: 'LoginAlreadySuccess',
+export const PayPayStatusBase = [
+  {
+    success: true,
+    status: 'LoginSuccess',
+  },
+  {
+    success: false,
+    status: 'LoginFailed',
+  },
+  {
+    success: false,
+    status: 'LoginIncorrectPassOrPhone',
+  },
+  {
+    success: false,
+    status: 'LoginNeedOTP',
+  },
+  {
+    success: true,
+    status: 'LoginDontNeedOTP',
+  },
+  {
+    success: true,
+    status: 'OTPLoginSuccess',
+  },
+  {
+    success: false,
+    status: 'OTPLoginFail',
+  },
+  {
+    success: true,
+    status: 'LoginAlreadySuccess',
+  }
+] as const
+
+const _PayPayStatus = PayPayStatusBase.reduce<Record<typeof PayPayStatusBase[number]['status'], string>>((acc, cur) => {
+  acc[cur.status] = cur.status
+  return acc
+}, {} as Record<typeof PayPayStatusBase[number]['status'], string>)
+
+export const PayPayStatus = _PayPayStatus
+
+export function statusof(status: typeof PayPayStatusBase[number]['status']): boolean {
+  const result = PayPayStatusBase.find((item) => item.status === status)
+
+  if (!result) return false
+
+  return result.success
 }
--- src/status/index.test.ts
+++ src/status/index.test.ts
@@ -1,10 +1,28 @@
 // PayPay Status Test
-import { PayPayStatus } from './index'
+import { PayPayStatus, statusof } from './index'
 
 describe('Status', () => {
   test('not include space', () => {
     for (const Status of Object.values(PayPayStatus)) {
       expect(Status).not.toMatch(/\s/)
     }
+  })
+
+  test('not include undefined or null', () => {
+    for (const Status of Object.values(PayPayStatus)) {
+      expect(Status).not.toBeUndefined()
+      expect(Status).not.toBeNull()
+    }
+  })
+
+  test('statusof', () => {
+    expect(statusof('LoginSuccess')).toBeTruthy()
+    expect(statusof('LoginFailed')).toBeFalsy()
+    expect(statusof('LoginDontNeedOTP')).toBeTruthy()
+    expect(statusof('LoginAlreadySuccess')).toBeTruthy()
+    expect(statusof('OTPLoginSuccess')).toBeTruthy()
+    expect(statusof('OTPLoginFail')).toBeFalsy()
+    expect(statusof('LoginIncorrectPassOrPhone')).toBeFalsy()
+    expect(statusof('LoginNeedOTP')).toBeFalsy()
   })
 })
